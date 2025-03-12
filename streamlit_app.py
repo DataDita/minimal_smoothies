c140732aced524461c6d1a387fecac3be08b65e5
DataDita <saba.safa@datadita.com>
2025-03-12 11:48:37 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -13,10 +14,8 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your smoothie will be:", name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
 
-
+session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
@@ -43,7 +42,10 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!,', icon="✅")
-#new
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
+
+
+
+
+
+
