303e06528580deb0d00fb4836c55089c34940934
DataDita <saba.safa@datadita.com>
2025-03-11 23:46:28 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -43,6 +43,11 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!,', icon="✅")
 
+#new section
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 
 
 
