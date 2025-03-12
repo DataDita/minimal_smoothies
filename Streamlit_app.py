adbea297a65ecd0f34e7ffbfa4c848ea95cdb2fc
DataDita <saba.safa@datadita.com>
2025-03-11 23:26:35 -0400

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -15,9 +15,9 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your smoothie will be:", name_on_order)
 
 
-#session = get_active_session()
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingrediants:'
@@ -46,7 +46,7 @@ if ingredients_list:
 #new
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
 
